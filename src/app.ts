fe08dadb484c56c204af12e45876bf47deae2a0f
Piyanshu Saini <priyanshusaininew@gmail.com>
2023-12-13 01:14:03 +0530

    Add hello endpoints

--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,23 @@ const port =process.env.PORT || 3000;
 
 // Middleware to parse JSON bodies
 app.use(bodyParser.json());
-app.use(router)
+app.use(router);
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.get('/api', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.get('/api/test', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.get('/api/hello', (req, res) => {
+  res.send('Hello World!');
+});
 
 
 // Start the server
